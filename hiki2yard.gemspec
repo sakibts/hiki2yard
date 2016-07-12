0538160d9781d23aff322ca34d311962a3e3a320
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-07-12 11:51:14 +0900

    first running version.

--- hiki2yard.gemspec
+++ hiki2yard.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Shigeto R. Nishitani"]
   spec.email         = ["shigeto_nishitani@me.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Support making docs from hiki to yard, wiki, on gem development directory.}
+  spec.description   = %q{Support making docs from hiki to yard, wiki, on gem development directory.}
+  spec.homepage      = "http://github.com/daddygongon/hiki2yard.wiki"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
@@ -30,4 +30,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "yard"
+  spec.add_development_dependency "hiki2md"
+  spec.add_development_dependency "mathjax-yard"
 end
--- lib/hiki2yard.rb
+++ lib/hiki2yard.rb
@@ -1,5 +1,79 @@
+# -*- coding: utf-8 -*-
+require 'optparse'
 require "hiki2yard/version"
 
 module Hiki2yard
-  # Your code goes here...
+  class Command
+    def self.run(argv=[])
+      new(argv).execute
+    end
+
+    def initialize(argv=[])
+      @argv = argv
+      @source_path = File.expand_path('..', __FILE__)
+      @target_path = Dir.pwd
+      p @base_name=File.basename(@target_path)
+      p @source_path,@target_path
+    end
+
+    def execute
+      @argv << '--help' if @argv.size==0
+      command_parser = OptionParser.new do |opt|
+        opt.on('-v', 'show program Version.') { |v|
+          opt.version = Hiki2yard::VERSION
+          puts opt.ver
+        }
+        opt.on('-i','--init','initialize hiki2yard directory.') { |v|
+          init
+          exit
+        }
+      end
+      command_parser.banner = "Usage: hiki2yard [options] FILE"
+      command_parser.parse!(@argv)
+      exit
+    end
+
+    def init
+      rev_rakefile
+      mk_yardopts
+      create_dir_if_not_exists(File.join(@target_path,'hikis'))
+      create_dir_if_not_exists(File.join(@target_path,"#{@base_name}.wiki"))
+      rev_gemspec
+    end
+
+    def rev_rakefile
+      source=File.join(@source_path,'hiki2yard','new_rakefile')
+      target=File.join(@target_path,'Rakefile')
+      copy_file_if_not_exists(source, target)
+    end
+
+    def mk_yardopts
+      cont = "-\n**/*.md\n"
+      File.write(File.join(@target_path,'.yardopts'),cont)
+    end
+
+    def create_dir_if_not_exists(data_path)
+      return if File::exists?(data_path)
+      FileUtils.mkdir(data_path,:verbose=>true) # :noop=>true)
+    end
+
+    def copy_file_if_not_exists(source, target)
+      return if File::exists?(target)
+      FileUtils.cp(source,target,:verbose=>true) # :noop=>true)
+    end
+
+    def rev_gemspec
+      cont=<<"EOS"
+  spec.add_development_dependency "yard"
+  spec.add_development_dependency "hiki2md"
+  spec.add_development_dependency "mathjax-yard"
+EOS
+      print "add follows at the tail of #{@target_path}/#{@base_name}.gemspec\n"
+      print cont
+    end
+  end
 end
+
+
+
+
